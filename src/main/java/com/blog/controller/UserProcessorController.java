f745259c6ddc25e40bed1a5dc4b6fb43f7fee5fa
FuNian Cui <857651870@qq.com>
2018-12-17 15:30:59 +0800

    新增User接口

--- src/main/java/com/blog/controller/UserProcessorController.java
+++ src/main/java/com/blog/controller/UserProcessorController.java
@@ -3,9 +3,12 @@ package com.blog.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blog.dto.UserResignDTO;
 import com.blog.vo.DefaultResultVO;
 
 /**
@@ -18,11 +21,9 @@ import com.blog.vo.DefaultResultVO;
 public class UserProcessorController {
 	private static final Logger logger = LogManager.getLogger(UserProcessorController.class);
 	
-	public DefaultResultVO addAdmin() {
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public DefaultResultVO addAdmin(@RequestBody UserResignDTO userResignDTO) {
 		logger.info("用户注册");
-		DefaultResultVO resp = new DefaultResultVO();
-		resp.success();
-		
-		return resp;
+		return DefaultResultVO.success();
 	}
 }
---
+++ src/main/java/com/blog/dto/UserResignDTO.java
@@ -0,0 +1,10 @@
+package com.blog.dto;
+
+/**
+ * @file UserResignDTO.java
+ * @author FuNian Cui
+ * @date 2018年12月17日 下午12:23:16
+ */
+public class UserResignDTO {
+
+}
