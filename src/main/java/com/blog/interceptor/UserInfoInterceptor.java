5709210ce9ca1cc837a73208275268bca0cc4762
FuNian Cui <857651870@qq.com>
2018-12-28 10:09:35 +0800

    修改@Before拦截器

--- src/main/java/com/blog/interceptor/UserInfoInterceptor.java
+++ src/main/java/com/blog/interceptor/UserInfoInterceptor.java
@@ -6,6 +6,8 @@ import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -13,6 +15,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.blog.annotation.Before;
 import com.blog.enums.ResultCodeEnum;
 import com.blog.util.GetRequestJSONUtils;
+import com.blog.util.HttpServletRequestWrapperUtil;
 import com.blog.util.UserInfoValidationUtil;
 import com.blog.vo.DefaultResultVO;
 
@@ -22,6 +25,7 @@ import com.blog.vo.DefaultResultVO;
  * @date 2018年12月25日 下午1:16:55
  */
 public class UserInfoInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger logger = LogManager.getLogger(UserInfoInterceptor.class);
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -30,25 +34,33 @@ public class UserInfoInterceptor extends HandlerInterceptorAdapter {
             return true;
         }
         
+        //获取请求参数
+    	//new HttpServletRequestWrapperUtil(request).getBodyString(request);
+        
         // 将handler强转为HandlerMethod, 前面已经证实这个handler就是HandlerMethod
         HandlerMethod handlerMethod = (HandlerMethod) handler;
         // 从方法处理器中获取出要调用的方法
         Method method = handlerMethod.getMethod();
         // 获取出方法上的Access注解
         Before before = method.getAnnotation(Before.class);
+        
         if (before == null) {
         	// 如果注解为null, 说明不需要拦截, 直接放过
             return true;
         } else {
         	//进行校验
+        	//ServletRequest的getReader()和getInputStream()两个方法只能被调用一次，而且不能两个都调用。
         	String jsonString = GetRequestJSONUtils.getRequestJsonString(request);
         	JSONObject jsonObject = JSONObject.parseObject(jsonString);
+        	logger.info("jsonObject：" + jsonObject);
+        	
         	if(!UserInfoValidationUtil.validationUserName(jsonObject.getString("userName"))) {
         		//重置response
                 response.reset();
                 //设置编码格式
                 response.setCharacterEncoding("UTF-8");
                 response.setContentType("application/json;charset=UTF-8");
+                //response.setHeader("Content-type", "application/json;charset=UTF-8");
                 
                 //返回前端错误信息
                 PrintWriter pw = response.getWriter();
@@ -59,6 +71,16 @@ public class UserInfoInterceptor extends HandlerInterceptorAdapter {
         		return false;
         	}
         }
-		return true;
+        
+        HttpServletRequestWrapperUtil requestWrapper = new HttpServletRequestWrapperUtil(request);
+        byte[] body = requestWrapper.getBody();
+        logger.info("请求Body: ", body);
+        // 默认记录后台接口请求日志记录
+        String url = request.getRequestURL().toString();
+        String methodName = request.getMethod();
+        String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+        logger.info(String.format("请求参数, url: %s, method: %s, uri: %s, params: %s ", url, methodName, uri, queryString));
+        return true;
 	}
 }
